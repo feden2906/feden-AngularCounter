b01d930b34ca69437d1d2b35df3b172fc9fe894b
Fedenko Maksym <feden2906@gmail.com>
2021-01-26 19:28:57 +0200

    styled counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { TitleComponent } from './components/title/title.component';
+import { ResultComponent } from './components/result/result.component';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    TitleComponent,
+    ResultComponent,
+    ButtonsComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule
